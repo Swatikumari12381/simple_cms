1a7dba320850908b68bab92657f458b0c8f1a57f
swati <swati.kumari12381@gmail.com>
2019-07-23 18:14:45 +0530

    3rd commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
-  
+
   get 'home/index'
-  get 'subjects/index1'
+  get 'subjects/index'
 
 end
