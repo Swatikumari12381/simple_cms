3013983b81897df4635a25de7e06a33c795d07b6
swati <swati.kumari12381@gmail.com>
2019-07-26 12:06:59 +0530

    including models and controller

--- db/migrate/20190726051709_create_authors.rb
+++ db/migrate/20190726051709_create_authors.rb
@@ -4,7 +4,7 @@ class CreateAuthors < ActiveRecord::Migration[5.2]
       t.string "email"
       t.string "first_name"
       t.string "last_name"
-      has_one :profile
+      
       t.timestamps
     end
   end
