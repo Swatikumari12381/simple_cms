5a4e000cb9ca0c081e55d9488aa61b40a6d7a8cb
swati <swati.kumari12381@gmail.com>
2019-08-05 11:39:49 +0530

    after making public view

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'pages/index'
   get 'authors/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  root 'public#index'
 
   get 'home/index'
 
